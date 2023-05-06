9925d67717711cf0b0e376c7f6a2931beb57aa9d
sunlit-ind <uekaram@gmail.com>
2023-05-06 18:24:58 +0900

    Calender01

---
+++ scheduleCalendar/static/scheduleCalendar/app.js
@@ -0,0 +1,79 @@
+// CSRF対策
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
+axios.defaults.xsrfCookieName = "csrftoken"
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+
+        // 日本語化
+        locale: 'ja',
+
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+        },
+
+        dayCellContent: function(e) {
+            e.dayNumberText = e.dayNumberText.replace('日', '');
+        },
+
+        // 日付をクリック、または範囲を選択したイベント
+        selectable: true,
+        select: function(info) {
+            //alert("selected " + info.startStr + " to " + info.endStr);
+
+            // 入力ダイアログ
+            const eventName = prompt("イベントを入力してください");
+
+            if (eventName) {
+
+                // 登録処理の呼び出し
+                axios
+                    .post("/scheduleCalendar/add/", {
+                        start_date: info.start.valueOf(),
+                        end_date: info.end.valueOf(),
+                        event_name: eventName,
+                    })
+                    .then(() => {
+                        // イベントの追加
+                        calendar.addEvent({
+                            title: eventName,
+                            start: info.start,
+                            end: info.end,
+                            allDay: true,
+                        });
+
+                    })
+                    .catch(() => {
+                        // バリデーションエラーなど
+                        alert("登録に失敗しました");
+                    });
+            }
+        },
+
+        events: function(info, successCallback, failureCallback) {
+
+            axios
+                .post("/scheduleCalendar/list/", {
+                    start_date: info.start.valueOf(),
+                    end_date: info.end.valueOf(),
+                })
+                .then((response) => {
+                    calendar.removeAllEvents();
+                    successCallback(response.data);
+                })
+                .catch(() => {
+                    // バリデーションエラーなど
+                    alert("登録に失敗しました");
+                });
+
+        },
+    });
+
+    calendar.render();
+});
